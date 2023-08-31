0c62fcb8393fe30dd3eb85c5e911fcb9e4fde755
SnehParatwar <paratwarsneh@gmail.com>
2023-08-31 10:51:29 +0530

    sneh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import About from './pages/AboutPage/About';
 
 function App() {
   return (
     <div className="App">
       <h1>Ranjeet</h1>
+      <About/>
     </div>
   );
 }
